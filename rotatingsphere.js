5dc1d45bc38558d5630cd822b70d1f30dfcf67fa
Aniisa <anibi335@student.liu.se>
2020-02-25 15:15:43 +0100

    Testing textures with a starry rotating background

---
+++ rotatingsphere.js
@@ -0,0 +1,40 @@
+var scene = new THREE.Scene();
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var ratio = window.innerWidth/window.innerHeight;
+var camera = new THREE.PerspectiveCamera(45, ratio, 1, 1000);
+camera.position.z = 5;
+camera.position.y = 2;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+//var controls = new THREE.OrbitControls( camera ); //Give user control of camera
+
+//Adding cube to scene
+var geometry = new THREE.SphereGeometry();
+var material = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
+var cube = new THREE.Mesh( geometry, material );
+cube.overdraw = true;
+scene.add( cube );
+
+//Adding directional light to scene
+var light = new THREE.DirectionalLight(0xFFFFFF, 1);
+var lightAngle = 0;
+light.position.set(0, 1, 0.5);
+scene.add(light);
+
+
+//Rendering 
+function animate() {
+	//Light moves in circular motion above cube
+	/*lightAngle += 2;
+		if (lightAngle > 360) { lightAngle = 0;};
+	  light.position.x = 2 * Math.cos(lightAngle * Math.PI / 180);
+	  light.position.z = 2 * Math.sin(lightAngle * Math.PI / 180);*/
+
+	requestAnimationFrame( animate );
+	cube.rotation.x += 0.001;
+	cube.rotation.y += 0.001;
+	renderer.render( scene, camera );
+}
+animate();
